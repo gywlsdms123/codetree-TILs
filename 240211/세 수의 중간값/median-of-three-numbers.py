d7151b4031e6697b066a887280f3e72d47cf58d9
HYOJIN <51704606+gywlsdms123@users.noreply.github.com>
2024-02-11 01:31:09 +0900

    Title: 세 수의 중간값, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240211/세 수의 중간값/median-of-three-numbers.py
+++ 240211/세 수의 중간값/median-of-three-numbers.py
@@ -1,3 +1,12 @@
-a,b,c = map(int, input().split())
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
 
-print("1" if c > b > a else "0")
+# 출력
+if b > a and c > b:
+	print(1)
+else:
+	print(0)
